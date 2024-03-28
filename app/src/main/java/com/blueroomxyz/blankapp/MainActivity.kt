08fce4f5a61d7de9e2567bb1fe182cc4c14bcda7
raza404y <raza404y@>
2024-03-28 19:05:32 +0500

    Initial Commit

--- app/src/main/java/com/blueroomxyz/blankapp/MainActivity.kt
+++ app/src/main/java/com/blueroomxyz/blankapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.blueroomxyz.blankapp
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.Window
+import android.view.WindowInsetsController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +13,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Transparent Status Bar
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
 
     }
 }
